4e9ef231c78739254f2d37873fd4a777f2471b9f
Joel <joel.harawa@gmail.com>
2024-01-03 07:52:37 +0300

    projects update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -81,32 +81,19 @@ const Home = () => {
         {name: 'CULTR Magazine', content: 'CULTR Magazine, tailored for a dynamic club at Michigan State' +
                 ' University, is made up of a ReactJS front-end crafted with a blend of JavaScript, HTML, and CSS. ' +
                 'The back-end server, powering the platform, is hosted on an AWS EC2 instance and built using NodeJS.'},
-        {name: 'Moosic', content: 'MOOSIC, is a ReactJS-based Spotify playlist generator, is a creative project' +
-                ' designed to curate personalized music experiences based on a user\'s mood. The front-end, developed with a mix of' +
-                ' JavaScript, HTML, and CSS, navigates the user through an intuitive playlist creation' +
-                ' process. Leveraging Spotify\'s API for seamless integration, MOOSIC generates playlists' +
+        {name: 'Moosic', content: 'MOOSIC, is a ReactJS-based Spotify playlist generator creating playlists based on a user\'s mood. The front-end, developed with a mix of' +
+                ' JavaScript, HTML, and CSS. Leveraging Spotify\'s API for seamless integration, MOOSIC generates playlists' +
                 ' tailored to individual preferences, promising a unique and enjoyable musical journey.'},
-        {name:'27 - Puzzle', content: 'In my Intro to Artificial Intelligence class, I successfully tackled the challenge of a 3-dimensional n-puzzle using C++. ' +
+        {name:'27 - Puzzle', content: 'In my Intro to Artificial Intelligence class, I tackled the challenge of a 3-dimensional n-puzzle using C++. ' +
                 'Employing the A-star search algorithm and leveraging the Manhattan distance heuristic, the solution' +
-                ' seamlessly navigates the puzzle\'s complexity. The result is an efficient and optimized algorithm' +
-                ' that adeptly solves the intricate puzzle, showcasing the power of C++ in algorithmic problem-solving'},
+                ' finds the most optimal path from initial state to goal state.'},
         {name: 'E20 Processor', content: 'I implemented an \'E20\' processor with cache using Python, a project' +
                 ' assigned by my Computer Architecture professor. This processor written in python' +
-                '  Python, mirrors the architecture envisioned, showcasing both my programming skills and a deep ' +
-                'understanding of computer architecture principles. The inclusion of a cache system further enhances' +
-                ' its efficiency, making it a testament to the fusion of theoretical knowledge and practical' +
-                ' application in the realm of processor design'},
-        {name: 'Piper', content: 'Piper is a minimalist Android music app, written in Java. The user interface is' +
-                ' designed to display a visually pleasing gradient derived from the album artwork.' +
-                ' Leveraging Java\'s versatility, Piper delivers a simple music experience on the Android platform,' +
-                ' where the dynamic color gradients based on album artwork add an extra layer of visual appeal to the' +
-                ' user interface.'},
-        {name: 'FlyGenie', content: 'FlyGenie is a comprehensive flight booking website crafted as part of my' +
-                ' Databases class project. Leveraging a robust tech stack, FlyGenie integrates ReactJS for a dynamic' +
-                ' front-end, NodeJS to power the back-end, and MySQL to manage the database. This trifecta of' +
-                ' technologies ensures a smooth and efficient user experience, from exploring flight options to making' +
-                ' reservations. FlyGenie not only exemplifies the application of database concepts but also showcases' +
-                ' the synergy of ReactJS and NodeJS in creating a user-friendly platform for flight bookings.'},
+                '  Python, mirrors the architecture envisioned. The inclusion of a cache system further enhances' +
+                ' its efficiency.'},
+        {name: 'FlyGenie', content: 'FlyGenie is a flight booking website crafted as part of my' +
+                ' Databases class project. Leveraging a simple tech stack, FlyGenie integrates ReactJS for a dynamic' +
+                ' front-end, NodeJS to power the back-end, and MySQL to manage the database.'},
     ];
 
     const toggleProject = (clickedProject) => {
@@ -146,7 +133,6 @@ const Home = () => {
                                     )}
                             </div>
                         ))}
-
                     </ProjectContainer>
                 </Wrapper>
             </Container>
