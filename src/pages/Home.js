a8c5fff1e29cae9c7a0a8f912e96d8f100b50ac5
Joel <joel.harawa@gmail.com>
2024-06-25 15:08:00 +0300

    minor update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
                     <InfoContainer>
                         <Info><span style={{color: '#7BC2BC'}}>About Me</span></Info>
                         <Info>Hello, I'm <span style={{color: '#7BC2BC'}}> Joel Harawa</span>, and I've recently graduated from New York
-                            University with a Bachelor's of Science in Computer Science. I'm an aspiring Software
+                            University with a Bachelor of Science in Computer Science. I'm an aspiring Software
                             engineer, with interests in both front-end and back-end engineering.
                             <br/>
                             <br/>
